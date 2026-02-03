d3036b1b1b3d5b891b446e0e5ad028b60c88013e
Daniel Quan <danielquan.c@gmail.com>
2026-02-02 20:54:15 -0600

    Update: Add the project updated at the moment

---
+++ classes/SuperHeroeProject/Clase2/Clase2/Interfaz/IServicioSuperHeroe.cs
@@ -0,0 +1,11 @@
+﻿using Clase2.Modelos;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Clase2.Interfaz
+{
+    public interface IServicioSuperHeroe
+    {
+        public IEnumerable<SuperHeroe> ObtenerSuperH(SqlConnection connection);
+    }
+}
---
+++ classes/SuperHeroeProject/Clase2/Clase2/Servicios/ServicioSuperHeroe.cs
@@ -0,0 +1,25 @@
+﻿using Clase2.Interfaz;
+using Clase2.Modelos;
+using System.Data.SqlClient;
+using Dapper;
+using System.Collections.Generic;
+using System;
+
+namespace Clase2.Servicios
+{
+    public class ServicioSuperHeroe : IServicioSuperHeroe
+    {
+        public IEnumerable<SuperHeroe> ObtenerSuperH(SqlConnection connection)
+        {
+            try
+            {
+                var resultado = connection.QueryAsync<SuperHeroe>("select * from superheroe");
+                return resultado.Result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener los superheroes ", ex);
+            }
+        }
+    }
+}
