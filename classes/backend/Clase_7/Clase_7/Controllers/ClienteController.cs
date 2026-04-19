1ccda3e27a14afacc85fd04a04cbf9d7ddfcad03
Daniel Quan <danielquan.c@gmail.com>
2026-04-19 07:54:14 -0600

    Update all existing projects

---
+++ classes/backend/Clase_7/Clase_7/Controllers/ClienteController.cs
@@ -0,0 +1,31 @@
+﻿using Clase_7.Interfaz;
+using Clase_7.Modelo;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+
+namespace Clase_7.Controllers
+{
+    [Produces("applications/json")]
+    [Route("api/[controller]")]
+    public class ClienteController
+    {
+        private readonly IConfiguration _config;
+        private readonly IClienteServicio _clienteServicio;
+        public ClienteController(IConfiguration config, IClienteServicio clienteServicio)
+        {
+            _config = config;
+            _clienteServicio = clienteServicio;
+        }
+
+
+        [HttpGet("ObtenerTodos")]
+        public async Task<ActionResult<List<Cliente>>> ObtenerTodos()
+        {
+            using var connection = new NpgsqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.OpenAsync();
+            var heroes = _clienteServicio.TodosLosClientes(connection);
+            return Ok(heroes);
+        }
+    }
+}
---
+++ classes/backend/Clase_7/Clase_7/Interfaz/IClienteServicio.cs
@@ -0,0 +1,11 @@
+﻿using Npgsql;
+using Clase_7.Modelo;
+
+namespace Clase_7.Interfaz
+{
+    public interface IClienteServicio
+    {
+
+        public IEnumerable<Cliente> TodosLosClientes(NpgsqlConnection connection);
+    }
+}
