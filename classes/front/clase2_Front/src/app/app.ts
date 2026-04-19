d39e583b9257c33374b5cebbdea0974246b13be7
Daniel Quan <danielquan.c@gmail.com>
2026-04-19 08:26:31 -0600

    update the project in class

--- classes/front/clase2_Front/src/app/app.ts
+++ classes/front/clase2_Front/src/app/app.ts
@@ -8,5 +8,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+  titulo = "clase 2 desarrollo web - Front end"
+  nombre = "Daniel"
+  apellido = "Latorre"
   protected readonly title = signal('clase2_Front');
+  DuplicarNumero(valor: number): number{
+  return valor*2;
+  }
+  SumarNumeros(valor1:number, valor2: number): number{
+    return valor1 + valor2
+  }
+  pelicula = {
+    titulo: "Spider-man",
+    FechaDeLanzamiento: new Date(),
+    precio: 5000
+  }
 }
+
